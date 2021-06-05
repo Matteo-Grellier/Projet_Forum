c92aa1e98063d0023f0a0823aa34f3f814f37ca5
Matteo-Grellier <matteo-g@orange.fr>
2021-06-05 15:24:05 +0200

    Update home.go + error404.html | Add error500.html
    Permet de gérer les erreurs 404 et 500 avec une page HTML personnaliser pour chacune des erreurs

--- handlers/home.go
+++ handlers/home.go
@@ -7,11 +7,22 @@ import (
 )
 
 func Home(w http.ResponseWriter, req *http.Request) {
+	arr := []string{"/", "/connexion", "/likedPosts", "/oneCategory", "/postsActivity", "/topic", "/inscription"}
 
 	t, err := template.ParseFiles("./templates/home.html", "./templates/layouts/sidebar.html", "./templates/layouts/header.html")
-	if req.URL.Path != "/" {
-		fmt.Printf("error 404 %s", err)
-		t, _ = template.ParseFiles("./templates/layouts/error.html")
+	if err != nil {
+		for i := 0; i < len(arr); i++ {
+			if req.URL.Path != arr[i] {
+				t, _ = template.ParseFiles("./templates/layouts/error404.html")
+				t.Execute(w, nil)
+				return
+			} else if req.URL.Path == arr[i] {
+				break
+			}
+		}
+	}
+	if err != nil {
+		t, _ = template.ParseFiles("./templates/layouts/error500.html")
 		t.Execute(w, nil)
 		return
 	}
