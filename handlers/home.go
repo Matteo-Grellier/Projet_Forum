048bff04f35902e706bdf0aaa64c72b64cb76cc5
Matteo-Grellier <matteo-g@orange.fr>
2021-06-05 15:42:52 +0200

    Update home.go
    Correction d'un bug qui activait les pages d'erreurs seulement s'il y avait un template défectueux

--- handlers/home.go
+++ handlers/home.go
@@ -10,15 +10,13 @@ func Home(w http.ResponseWriter, req *http.Request) {
 	arr := []string{"/", "/connexion", "/likedPosts", "/oneCategory", "/postsActivity", "/topic", "/inscription"}
 
 	t, err := template.ParseFiles("./templates/home.html", "./templates/layouts/sidebar.html", "./templates/layouts/header.html")
-	if err != nil {
-		for i := 0; i < len(arr); i++ {
-			if req.URL.Path != arr[i] {
-				t, _ = template.ParseFiles("./templates/layouts/error404.html")
-				t.Execute(w, nil)
-				return
-			} else if req.URL.Path == arr[i] {
-				break
-			}
+	for i := 0; i < len(arr); i++ {
+		if req.URL.Path != arr[i] {
+			t, _ = template.ParseFiles("./templates/layouts/error404.html")
+			t.Execute(w, nil)
+			return
+		} else if req.URL.Path == arr[i] {
+			break
 		}
 	}
 	if err != nil {
