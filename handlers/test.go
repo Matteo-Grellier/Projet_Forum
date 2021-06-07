aa17f3726c506ea5bed049681a86d5d8ca6c1232
OMOREAU <moreauolivia49@gmail.com>
2021-06-07 10:56:31 +0200

    udpate index.html + test.go | create BDDFroum1.db + bdd.sql + FORUM_BDD.dbs
    BDD fonctionnelle, problème résolu, tout s'affiche.

--- handlers/test.go
+++ handlers/test.go
@@ -18,10 +18,10 @@ type Data struct {
 	Users []User
 }
 func Afficher(w http.ResponseWriter, req *http.Request){
-	t, _ := template.ParseFiles("./template/index.html")
+	t, _ := template.ParseFiles("./templates/index.html")
 
 	/*fonction base de données*/
-	db, err := sql.Open("sqlite3", "../BDDForum1.db")
+	db, err := sql.Open("sqlite3", "./BDD/BDDForum1.db")
 	if (err!=nil){
 		fmt.Println("Could Not open Database")
 	}
@@ -39,12 +39,34 @@ func Afficher(w http.ResponseWriter, req *http.Request){
 	dataOk := Data{
 		Users : bdd(db),
 	}
+	fmt.Println(dataOk)
 	t.Execute(w, dataOk)
+}
+
+
+func bdd(db *sql.DB) []User {
+	
+	var eachUser User
+	var tabUsers []User
+	entries, err := db.Query("SELECT pseudo, mail FROM `user`")
+
+	if (err!=nil){
+		fmt.Println("Could not query database")
+		log.Fatal(err)
+		// return
+	}
+	for entries.Next(){
+		entries.Scan(&eachUser.Pseudo, &eachUser.Mail)
+		tabUsers = append(tabUsers, eachUser)
+		fmt.Println(tabUsers)
+	}
+
+	return tabUsers
 }
 
 func update(db *sql.DB){
 	update, err := db.Prepare("UPDATE user SET pseudo = ? WHERE pseudo = ?")
-	update.Exec("liv44", "liv49")
+	update.Exec("testUdpate", "test3")
 	if err != nil {
 		fmt.Println(err)
 	} else if update == nil {
@@ -61,23 +83,4 @@ func delete(db *sql.DB){
 	delete, _ := db.Prepare("DELETE FROM user WHERE pseudo = ?")
 	delete.Exec("Mat")
 
-}
-
-func bdd(db *sql.DB) []User {
-	
-	var eachUser User
-	var tabUsers []User
-	entries, err := db.Query("SELECT * FROM post")
-
-	if (err!=nil){
-		fmt.Println("Could not query database")
-		log.Fatal(err)
-		// return
-	}
-	for entries.Next(){
-		entries.Scan(&eachUser.Pseudo, &eachUser.Mail)
-		tabUsers = append(tabUsers, eachUser)
-	}
-
-	return tabUsers
 }
