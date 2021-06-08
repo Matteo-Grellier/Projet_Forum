bf5223e18eb688032a85a5aa8ab38c6c04ecde94
root <root@MSI.localdomain>
2021-06-08 09:05:33 +0200

    Update all_categories.html
    Modification du bouton en enlevant la barre violette qui se situait en dessous

--- static/js/all_categories.js
+++
@@ -1,4 +0,0 @@
-// This is just to auto-update the data-text if you're editing it directly on the page and is not required for the actual effect
-$('[data-text]').on('keyup', function(){
-    $(this).attr('data-text', $(this).text());
-  });
