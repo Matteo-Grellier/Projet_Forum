b659c400743c93909409242eee8e8c5e17106c17
root <root@MSI.localdomain>
2021-06-14 14:19:44 +0200

    Update all_categories.html, all_categories.css, all_categories.go
    Implementation de la zone pour toute les catégories

--- handlers/all_Categories.go
+++ handlers/all_Categories.go
@@ -9,11 +9,11 @@ import (
 
 func All_Categories(w http.ResponseWriter, r *http.Request) {
 	// Déclaration des fichiers à parser
-	t, err := template.ParseFiles("templates/all_categories.html", "templates/layouts/sidebar.html")
+	t, err := template.ParseFiles("templates/all_categories.html", "templates/layouts/sidebar.html", "templates/layouts/header.html")
 	if err != nil {
 		log.Fatalf("Template execution: %s", err)
 		return
 	}
-	fmt.Println("Page Connexion ✅")
+	fmt.Println("Page All Catégories ✅")
 	t.Execute(w, nil)
 }
