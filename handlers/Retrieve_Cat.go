a1d74437703dfd621c5da9d8eea585ed5da0779d
Matteo-Grellier <matteo-g@orange.fr>
2021-06-15 08:45:48 +0200

    Update server.go + all_categories.html | Add Retrieve_Cat.go
    Récupération des categories contenu dans la BDD et affichage sur la page all_cateries

---
+++ handlers/Retrieve_Cat.go
@@ -0,0 +1,55 @@
+package handlers
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"net/http"
+	"text/template"
+
+	_ "github.com/mattn/go-sqlite3"
+)
+
+type Category struct {
+	Name string
+}
+
+type Data struct {
+	Categories []Category
+}
+
+func RetrieveCat(w http.ResponseWriter, req *http.Request) {
+	t, _ := template.ParseFiles("./templates/all_categories.html")
+
+	/*fonction base de données*/
+	db, err := sql.Open("sqlite3", "./BDD/BDDForum1.db")
+	if err != nil {
+		fmt.Println("Could Not open Database")
+	}
+
+	dataOk := Data{
+		Categories: bdd(db),
+	}
+	fmt.Println(dataOk)
+	t.Execute(w, dataOk)
+}
+
+func bdd(db *sql.DB) []Category {
+
+	var eachCategory Category
+	var tabCategorys []Category
+	entries, err := db.Query("SELECT name FROM category")
+
+	if err != nil {
+		fmt.Println("Could not query database")
+		log.Fatal(err)
+		// return
+	}
+	for entries.Next() {
+		entries.Scan(&eachCategory.Name)
+		tabCategorys = append(tabCategorys, eachCategory)
+		fmt.Println(tabCategorys)
+	}
+
+	return tabCategorys
+}
