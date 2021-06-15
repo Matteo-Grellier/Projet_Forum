824a2c968d2e7547503a6db4f55492fe8145e565
ByMSRT <73824665+ByMSRT@users.noreply.github.com>
2021-06-15 14:51:39 +0200

    Update one_category.go + server.go + one_category.html

--- handlers/one_category.go
+++ handlers/one_category.go
@@ -5,15 +5,47 @@ import (
 	"log"
 	"net/http"
 	"text/template"
+
+	BDD "../BDD"
 )
+
+type DataUsed struct {
+	Topics []Topic
+}
+
+type Topic struct {
+	Title       string
+	Content     string
+	User_pseudo string
+}
 
 func One_Category(w http.ResponseWriter, r *http.Request) {
 	// Déclaration des fichiers à parser
 	t, err := template.ParseFiles("templates/one_category.html", "templates/layouts/sidebar.html", "./templates/layouts/header.html")
+	DataUsedOK := DataUsed{
+		Topics: DisplayTopics(),
+	}
+
 	if err != nil {
 		log.Fatalf("Template execution: %s", err)
 		return
 	}
 	fmt.Println("Page Connexion ✅")
-	t.Execute(w, nil)
+	t.Execute(w, DataUsedOK)
+}
+
+func DisplayTopics() []Topic {
+	db := BDD.OpenDataBase()
+	var eachTopic Topic
+	var tabTopics []Topic
+	topics, err := db.Query("SELECT title, content, user_pseudo FROM topic")
+	if err != nil {
+		fmt.Println("Could not query database")
+		log.Fatal(err)
+	}
+	for topics.Next() {
+		topics.Scan(&eachTopic.Title, &eachTopic.Content, &eachTopic.User_pseudo)
+		tabTopics = append(tabTopics, eachTopic)
+	}
+	return tabTopics
 }
