86ee896b02e4c9ed4210d8cd24b5441427656a4f
Matteo-Grellier <matteo-g@orange.fr>
2021-06-22 13:49:13 +0200

    Update topic.go + topic.css + topic.html
    Mise en place du css sur la page Topic

--- handlers/topic.go
+++ handlers/topic.go
@@ -8,7 +8,7 @@ import (
 
 func TopicPage(w http.ResponseWriter, r *http.Request) {
 	// Déclaration des fichiers à parser
-	t, err := template.ParseFiles("templates/topic.html", "templates/layouts/sidebar.html")
+	t, err := template.ParseFiles("templates/topic.html", "templates/layouts/sidebar.html", "./templates/layouts/header.html")
 	if err != nil {
 		Color(3, "[SERVER_INFO_PAGE] : 🟠 Template execution : ")
 		log.Fatalf("%s", err)
