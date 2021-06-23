8b2d475ba123dc7f1e87a048a959892cb6753557
Matteo-Grellier <matteo-g@orange.fr>
2021-06-23 12:48:43 +0200

    Update handlers.go + Comment.js + topic.html
    Réglage de petits bugs

--- static/js/Comments.js
+++ static/js/Comments.js
@@ -1,10 +1,8 @@
 function createDiv() {
     if (document.body.contains(document.getElementById('notvisible'))) {
-        alert("euuuhh")
         document.getElementById('notvisible').setAttribute("id", "visible")
         var del = document.getElementById('visible').setAttribute("style", "");
     } else {
-        alert("yes")
         document.getElementById('visible').setAttribute("id", "notvisible")
         document.getElementById("notvisible");
         rep = document.getElementById('notvisible').setAttribute("style", "display:none;");
