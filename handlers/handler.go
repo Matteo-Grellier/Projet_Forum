eacb39f8b6fba84948795aabb0ade219a67e2afe
ByMSRT <73824665+ByMSRT@users.noreply.github.com>
2021-06-25 09:27:07 +0200

    Update handler.go + boxPost.css + topic.css + popup.js + topic.html | Add boxComm.css + boxComm.html
    Ajout d'une popup pour les commentaires, et refonte des fichiers pour la compatibilité

--- handlers/handler.go
+++ handlers/handler.go
@@ -158,7 +158,7 @@ func OneCategoryPage(w http.ResponseWriter, r *http.Request) {
 //Exécution de la page Topic
 func TopicPage(w http.ResponseWriter, r *http.Request) {
 	// Déclaration des fichiers à parser
-	t, err := template.ParseFiles("templates/topic.html", "templates/layouts/sidebar.html", "./templates/layouts/header.html", "./templates/layouts/boxPost.html")
+	t, err := template.ParseFiles("templates/topic.html", "templates/layouts/sidebar.html", "./templates/layouts/header.html", "./templates/layouts/boxPost.html", "./templates/layouts/boxComm.html")
 	if err != nil {
 		Color(3, "[SERVER_INFO_PAGE] : 🟠 Template execution : ")
 		log.Fatalf("%s", err)
