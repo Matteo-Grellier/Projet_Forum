fecafa3aad139c7b976c2f2aacc9d9ce5fdc1f29
Malo Loyer Viaud <39632426+Karrwolf@users.noreply.github.com>
2021-06-28 11:51:41 +0200

    Propreté du code

--- handlers/errors.go
+++ handlers/errors.go
@@ -40,32 +40,6 @@ func Error500(w http.ResponseWriter, req *http.Request, err error) {
 	t.Execute(w, nil)
 }
 
-/*// indexHandler - Serve INDEX.HTML page for 'HTTP GET /' request
-//
-func indexHandler(w http.ResponseWriter, r *http.Request) {
 
-	// HTTP/404 - Not Found management
-	if r.URL.Path != "/" {
-		http.Error(w, http.StatusText(http.StatusNotFound), http.StatusNotFound)
-		logger.Printf("[HTTP/404] - Page not found URL='%s'\n", r.URL.Path)
-		return
-	}
-
-	// Load banner list from COMMON declaration
-	tmpBannerList := make([]string, len(bannerSupported))
-	indexBanner := 0
-	for key := range bannerSupported {
-		tmpBannerList[indexBanner] = key
-		indexBanner++
-	}
-
-	// Initialize dynamic content of the page
-	htmlData := HTMLData{
-		Title:       "YTrack - Ascii-art-web",
-		Description: "Simple Golang webserver with built in logging, tracing and health check",
-		Keywords:    "golang web server ascii art",
-		Author:      "Cedric OBEJERO, <cedric.obejero@tanooki.fr>",
-		BannerFiles: tmpBannerList,
 		ASCIIText:   template.HTML(``),
 	}
-}*/
