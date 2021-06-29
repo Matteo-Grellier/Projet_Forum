63457fd3dba473a40407c314c9e49b2c9cd19835
OMOREAU <moreauolivia49@gmail.com>
2021-06-29 23:27:37 +0200

    update fichiers html + fichiers go
    Ajout de la section actus avec les 3 derniers posts ajoutés + création et affichage des posts likés par l'utilisateur(revoir le CSS)

--- handlers/errors.go
+++ handlers/errors.go
@@ -8,7 +8,7 @@ import (
 
 func Error404(w http.ResponseWriter, req *http.Request) bool {
 
-	arr := []string{"/", "/connexion", "/likedPosts", "/oneCategory", "/postsActivity", "/topic", "/inscription", "/test", "/categories"}
+	arr := []string{"/", "/connexion", "/likedPosts", "/oneCategory", "/postsActivity", "/topic", "/inscription", "/test", "/categories", "/likes"}
 	compteurURL := 0
 	for i := 0; i < len(arr); i++ {
 		if req.URL.Path != arr[i] {
