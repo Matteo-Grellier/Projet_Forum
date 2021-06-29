1cbef13a10f40d4cd4ca2d01be4f3ac65ad775a1
OMOREAU <moreauolivia49@gmail.com>
2021-06-29 09:31:54 +0200

    update Comments.js
    Pb d'affichage des coms OK

--- static/js/Comments.js
+++ static/js/Comments.js
@@ -1,8 +1,8 @@
 function createDiv(idPost) {
     var del = document.getElementById('visible_'+idPost)
     if (del.getAttribute("style") == "display:none"){
-        del.setAttribute("style", "display:null;");
+        del.setAttribute("style", "display:null");
     } else {
-        del.setAttribute("style", "display:none;");
+        del.setAttribute("style", "display:none");
     }
 }
